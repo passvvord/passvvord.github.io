0f9658ff5846de1648ea3b30cfd6fa37577c9a1d
passvvord <60948866+passvvord@users.noreply.github.com>
2020-03-31 20:27:35 +0300

    ....

--- labs/07/code/script.js
+++ labs/07/code/script.js
@@ -1,6 +1,6 @@
 let avatars = ["avatars/angel.svg","avatars/bag-on-head.svg","avatars/bionic-eye.svg","avatars/boy.svg","avatars/boy-1.svg","avatars/boy-2.svg","avatars/boy-3.svg","avatars/boy-4.svg","avatars/boy-5.svg","avatars/boy-6.svg","avatars/boy-7.svg","avatars/cat-mask.svg","avatars/dracula.svg","avatars/frankestein-monster.svg","avatars/girl.svg","avatars/girl-1.svg","avatars/girl-2.svg","avatars/girl-3.svg","avatars/girl-4.svg","avatars/girl-5.svg","avatars/girl-6.svg","avatars/girl-7.svg","avatars/girl-8.svg","avatars/girl-9.svg","avatars/girl-10.svg","avatars/girl-11.svg","avatars/girl-12.svg","avatars/girl-13.svg","avatars/girl-14.svg","avatars/girl-with-cat-ears.svg","avatars/hanibal-lecter.svg","avatars/man.svg","avatars/man-1.svg","avatars/man-2.svg","avatars/man-3.svg","avatars/man-4.svg","avatars/man-5.svg","avatars/man-6.svg","avatars/man-7.svg","avatars/man-8.svg","avatars/mujer.svg","avatars/mujer-1.svg","avatars/ninja.svg","avatars/nun.svg","avatars/pirate.svg","avatars/superheroe.svg","avatars/walter-white.svg","avatars/woman.svg","avatars/woman-1.svg","avatars/woman-2.svg"];
 let avatar = "avatars/ADMIN.svg";
-let us_name = 'heh';
+let us_name = '';
 let text = '';
 let count_likes = 0;
 let name_create = 0;
@@ -8,7 +8,7 @@ let name_create = 0;
 const database = firebase.database();
 const path = '/github/github_io/lab_07_data/';
 
-function print_coment_in(id,avatar,name,text,like) {
+function print_coment_in(id,avatar,name,text,like,path_name) {
 	let element = document.getElementById(id);
 	let element_str = "";
 	
@@ -17,7 +17,7 @@ function print_coment_in(id,avatar,name,text,like) {
 	+ `<div class="text-block">`
 	+ `<div class="name">`+name+`</div>`
 	+ `<div class="text">`+text+`</div>`
-	+ `<div class="likes">❤ `+like+`</div>`
+	+ `<div class="likes" onclick="like_up(`+ path_name +`)">❤ `+like+`</div>`
 	+ `</div>`
 	+ `</div>`;
 	element.innerHTML += element_str;
@@ -28,14 +28,27 @@ function add_coment() {
 	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
+
+
 	database.ref(path+name_create).set({
 		avatar: avatar,
 		name: us_name,
 		comment: text,
 		likes: count_likes,
 		date: date,
-		time: time
+		time: time,
+		display_w: document.documentElement.clientWidth,
+		display_h: document.documentElement.clientHeight
 	});
+}
+
+function like_up(path_name) {
+	let now_likes = 0;
+database.ref(path + path_name).once("value", function(snapshot) {
+	console.log('now_likes = ',snapshot.val().likes);
+	now_likes = parseInt(snapshot.val().likes) + 1;
+	});
+database.ref(path + path_name).update({likes: now_likes});
 }
 
 // function print_avatars_in(id) {
@@ -71,10 +84,9 @@ database.ref(path).orderByKey().on('value', snapshot => {
 			,snapshot.val()[i]['avatar']
 			,snapshot.val()[i]['name']
 			,snapshot.val()[i]['comment']
-			,snapshot.val()[i]['likes']);
-	}
-
-	console.log('count = ',comments_counter)
+			,snapshot.val()[i]['likes']
+			,i);
+	}	
 });
 
 
