39db194af266ab00481c55bf0917a7c556ad4d18
passvvord <60948866+passvvord@users.noreply.github.com>
2020-03-31 19:25:31 +0300

    little works on js

--- labs/07/code/script.js
+++ labs/07/code/script.js
@@ -1,8 +1,9 @@
-let avatars = ["angel.svg","bag-on-head.svg","bionic-eye.svg","boy.svg","boy-1.svg","boy-2.svg","boy-3.svg","boy-4.svg","boy-5.svg","boy-6.svg","boy-7.svg","cat-mask.svg","dracula.svg","frankestein-monster.svg","girl.svg","girl-1.svg","girl-2.svg","girl-3.svg","girl-4.svg","girl-5.svg","girl-6.svg","girl-7.svg","girl-8.svg","girl-9.svg","girl-10.svg","girl-11.svg","girl-12.svg","girl-13.svg","girl-14.svg","girl-with-cat-ears.svg","hanibal-lecter.svg","man.svg","man-1.svg","man-2.svg","man-3.svg","man-4.svg","man-5.svg","man-6.svg","man-7.svg","man-8.svg","mujer.svg","mujer-1.svg","ninja.svg","nun.svg","pirate.svg","superheroe.svg","walter-white.svg","woman.svg","woman-1.svg","woman-2.svg"];
-let avatar = "DEFAULT.svg";
+let avatars = ["avatars/angel.svg","avatars/bag-on-head.svg","avatars/bionic-eye.svg","avatars/boy.svg","avatars/boy-1.svg","avatars/boy-2.svg","avatars/boy-3.svg","avatars/boy-4.svg","avatars/boy-5.svg","avatars/boy-6.svg","avatars/boy-7.svg","avatars/cat-mask.svg","avatars/dracula.svg","avatars/frankestein-monster.svg","avatars/girl.svg","avatars/girl-1.svg","avatars/girl-2.svg","avatars/girl-3.svg","avatars/girl-4.svg","avatars/girl-5.svg","avatars/girl-6.svg","avatars/girl-7.svg","avatars/girl-8.svg","avatars/girl-9.svg","avatars/girl-10.svg","avatars/girl-11.svg","avatars/girl-12.svg","avatars/girl-13.svg","avatars/girl-14.svg","avatars/girl-with-cat-ears.svg","avatars/hanibal-lecter.svg","avatars/man.svg","avatars/man-1.svg","avatars/man-2.svg","avatars/man-3.svg","avatars/man-4.svg","avatars/man-5.svg","avatars/man-6.svg","avatars/man-7.svg","avatars/man-8.svg","avatars/mujer.svg","avatars/mujer-1.svg","avatars/ninja.svg","avatars/nun.svg","avatars/pirate.svg","avatars/superheroe.svg","avatars/walter-white.svg","avatars/woman.svg","avatars/woman-1.svg","avatars/woman-2.svg"];
+let avatar = "avatars/ADMIN.svg";
 let us_name = 'heh';
 let text = '';
 let count_likes = 0;
+let name_create = 0;
 
 const database = firebase.database();
 const path = '/github/github_io/lab_07_data/';
@@ -12,7 +13,7 @@ function print_coment_in(id,avatar,name,text,like) {
 	let element_str = "";
 	
 	element_str += `<div class="comment-div">`
-	+ `<div class="img-div"><img src="avatars/`+avatar+`"></div>`
+	+ `<div class="img-div"><img src="`+avatar+`"></div>`
 	+ `<div class="text-block">`
 	+ `<div class="name">`+name+`</div>`
 	+ `<div class="text">`+text+`</div>`
@@ -27,7 +28,7 @@ function add_coment() {
 	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-	database.ref(path+us_name).set({
+	database.ref(path+name_create).set({
 		avatar: avatar,
 		name: us_name,
 		comment: text,
@@ -35,11 +36,23 @@ function add_coment() {
 		date: date,
 		time: time
 	});
-
 }
 
-
-
+// function print_avatars_in(id) {
+// 	let element = document.getElementById(id);
+// 	let element_str = "";
+// 	for (let i of avatars) {
+// 		element_str += `<div class="comment-div">`
+// 		+ `<div class="img-div"><img src="avatars/`+i+`"></div>`
+// 		+ `<div class="text-block">`
+// 		+ `<div class="name">`+name+`</div>`
+// 		+ `<div class="text">`+text+`</div>`
+// 		+ `<div class="likes">❤ `+count_likes+`</div>`
+// 		+ `</div>`
+// 		+ `</div>`;
+// 	}
+// 	element.innerHTML += element_str;
+// }
 
 database.ref(path).orderByKey().on('value', snapshot => {
 
@@ -47,14 +60,21 @@ database.ref(path).orderByKey().on('value', snapshot => {
 	console.log(snapshot.val());
 
 	document.getElementById('comments').innerHTML = '';
+	name_create = 0;
+
+
 	for (let i in snapshot.val()) {
-		// console.log(snapshot.val()[i])
+		// console.log("i = ",i);
+		if (name_create <= i) { name_create = parseInt(i) + 1;}
+
 		print_coment_in('comments'
 			,snapshot.val()[i]['avatar']
 			,snapshot.val()[i]['name']
 			,snapshot.val()[i]['comment']
 			,snapshot.val()[i]['likes']);
 	}
+
+	console.log('count = ',comments_counter)
 });
 
 
