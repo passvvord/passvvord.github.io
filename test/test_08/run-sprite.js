4ca58cae22908ed2c3127e54fe5c8924090e1c63
passvvord <60948866+passvvord@users.noreply.github.com>
2020-04-16 05:28:04 +0300

    ...

--- test/test_08/run-sprite.js
+++ test/test_08/run-sprite.js
@@ -6,47 +6,68 @@ let aviable_sprites = {};
 class RunSprite extends HTMLElement {
 	constructor() {
 		super();
-
 		if (this.hasAttribute('img') && this.hasAttribute('frame_delay')) {
+			set_sprite(this,this.getAttribute('img'),this.getAttribute('frame_delay'));
+		}
+	}
+}
+customElements.define('run-sprite', RunSprite);
 
-			let sprite_src = this.getAttribute('img');
-			let css_var_name = this.getAttribute('img').replace(/[/._:;=()]/g,'-');
+function set_sprite(element, new_url, new_delay) {
+	if (element.hasAttribute('img') && element.hasAttribute('frame_delay') && !Number.isNaN(element.getAttribute('frame_delay')) ) {
+		if (element.getAttribute('img') != new_url || element.getAttribute('frame_delay') != new_delay || element.getAttribute('new') == 'true') {
+
+			element.setAttribute('new','false');
+
+			let old_src = element.getAttribute('img');
+			let old_var_name = 'var_'+old_src.replace(/[/.:;=()]/g, '_')+'_'+element.getAttribute('frame_delay');
+			element.setAttribute('img',new_url);
+			element.setAttribute('frame_delay',new_delay);
+
+			let var_name = 'var_'+new_url.replace(/[/.:;=()]/g, '_')+'_'+new_delay;
+
+			element.style.backgroundImage = 'url(' + new_url + ')';
 
 			let is_new = true;
 
 			for (let i in aviable_sprites) {
-				if (i == sprite_src) {
+				if (i == var_name) {
 					is_new = false;
 					break;
 				}
 			}
 
 			if (is_new) {
-				aviable_sprites[sprite_src] = {};
-				aviable_sprites[sprite_src].delay = this.getAttribute('frame_delay');
-				aviable_sprites[sprite_src].bg_x_pos = 0;
-				// aviable_sprites[sprite_src].bg_y_pos = 0;
-				aviable_sprites[sprite_src].size = Math.round(this.getBoundingClientRect().height);
-				document.documentElement.style.setProperty('--'+css_var_name+'-bg_x_pos',aviable_sprites[sprite_src].bg_x_pos + 'px');
-
-				let element = this;
+				aviable_sprites[var_name] = {};
+				aviable_sprites[var_name].src = new_url;
+				aviable_sprites[var_name].this_sprites_count = 0;
+				aviable_sprites[var_name].delay = new_delay;
+				aviable_sprites[var_name].bg_x_pos = 0;
+				// aviable_sprites[var_name].bg_y_pos = 0;
+				// if (element.hasAttribute('size')) {
+				// 	aviable_sprites[var_name].size = element.getAttribute('size');	
+				// } else {
+					aviable_sprites[var_name].size = Math.round(element.getBoundingClientRect().height);
+				// }
 
 				let img = new Image();
 
 				img.onload = function() {
-					
-					aviable_sprites[sprite_src].frames_count = Math.round(this.width/this.height);
-					
-					if (aviable_sprites[sprite_src].frames_count > 1) {
 
-						let sprite_anim = setInterval( ()=> {
-							if (aviable_sprites[sprite_src].bg_x_pos > -1*aviable_sprites[sprite_src].size*aviable_sprites[sprite_src].frames_count) {
-								aviable_sprites[sprite_src].bg_x_pos += -1*aviable_sprites[sprite_src].size;
+					aviable_sprites[var_name].frames_count = Math.round(this.width / this.height);
+
+					if (aviable_sprites[var_name].frames_count > 1 && aviable_sprites[var_name].delay != 0) {
+
+						aviable_sprites[var_name].setInterval_id = var_name;
+
+						window[var_name] = setInterval(() => {
+							if (aviable_sprites[var_name].bg_x_pos > -1 * aviable_sprites[var_name].size * aviable_sprites[var_name].frames_count) {
+								aviable_sprites[var_name].bg_x_pos += -1 * aviable_sprites[var_name].size;
 							} else {
-								aviable_sprites[sprite_src].bg_x_pos = 0;
+								aviable_sprites[var_name].bg_x_pos = 0;
 							}
-							document.documentElement.style.setProperty('--'+css_var_name+'-bg_x_pos',aviable_sprites[sprite_src].bg_x_pos + 'px');			
-						},aviable_sprites[sprite_src].delay)
+							document.documentElement.style.setProperty('--' + var_name + '-bg_x_pos', aviable_sprites[var_name].bg_x_pos + 'px');
+						}, aviable_sprites[var_name].delay)
 					}
 				}
 
@@ -54,48 +75,26 @@ class RunSprite extends HTMLElement {
 					element.style.backgroundImage = 'url(Error.gif)';
 				}
 
-				img.src = sprite_src;
+				img.src = aviable_sprites[var_name].src;
 			}
 
-			this.style.backgroundImage = 'url('+sprite_src+')';
-			this.style.height = aviable_sprites[sprite_src].size + 'px';
-			this.style.width = aviable_sprites[sprite_src].size + 'px';
-			this.style.imageRendering = 'pixelated';
-			this.style.backgroundSize = 'cover';
-			this.style.setProperty('background-position','var(--'+css_var_name+'-bg_x_pos) 0px');
+			aviable_sprites[var_name].this_sprites_count += 1;
+
+			if (var_name != old_var_name) {
+				aviable_sprites[old_var_name].this_sprites_count -= 1;
+				if (aviable_sprites[old_var_name].this_sprites_count == 0) {
+					clearInterval(window[aviable_sprites[old_var_name].setInterval_id]);
+					delete aviable_sprites[old_var_name];
+				}
+			}
+
+			element.style.height = aviable_sprites[var_name].size + 'px';
+			element.style.width = aviable_sprites[var_name].size + 'px';
+			element.style.imageRendering = 'pixelated';
+			element.style.backgroundSize = 'cover';
+			element.style.setProperty('background-position', 'var(--' + var_name + '-bg_x_pos) 0px');
+		
+			//console.log(element,new_url,new_delay,aviable_sprites[var_name]);
 		}
 	}
 }
-
-customElements.define('run-sprite', RunSprite);
-
-// function change_img(element,new_url,new_delay) {
-// 	// clearInterval(sprite_anim);
-
-// 	let img = new Image();
-
-// 	img.onload = function() {
-// 		element.shadowRoot.firstChild.style.backgroundImage = 'url('+new_url+')';
-// 		frames_count = Math.round(this.width/this.height);
-	
-// 		if (frames_count > 1) {
-// 			let bg_pos = 0;
-		
-// 			let sprite_anim = setInterval( ()=> {
-// 				if (bg_pos > -1*one_farme_size*frames_count) {
-// 					bg_pos+=-1*one_farme_size;
-// 				} else {
-// 					bg_pos = 0;
-// 				}
-		
-// 				element.shadowRoot.firstChild.style.backgroundPosition = bg_pos + 'px 0px';			
-// 			},new_delay)
-// 		}
-// 	}
-
-// 	img.onerror = function() {
-// 		element.shadowRoot.firstChild.style.backgroundImage = 'url(Error.gif)';
-// 	}
-
-// 	img.src = new_url;
-// }
