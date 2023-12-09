48362fdee4a0c9b4922cc9a7533dcf2cab098166
passvvord <60948866+passvvord@users.noreply.github.com>
2023-12-09 07:57:29 +0200

    update

--- test/test_12/js/global.js
+++ test/test_12/js/global.js
@@ -21,28 +21,59 @@ function animateSlider(min,max,func,speed = 2,call = 20) {
 
 window.AutoSpinSetInterval = null;
 
+const get3DmatrixFrom2D = (A)=>[A[0],A[1],0,0 ,A[2],A[3],0,0 ,0,0,1,0 ,A[4],A[5],0,1]
+
+function get3DMatrixTransform(el) {
+	const tempStringMatrix = getComputedStyle(el)['transform']
+	if (tempStringMatrix.includes('3d')) {
+		//              'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' -> [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+		return tempStringMatrix.slice(9,-1).split(',').map(a=>parseFloat(a));
+	} else if (tempStringMatrix != 'none') {
+		//              'matrix(1, 0, 0, 1, 0, 0)' -> [1, 0, 0, 1, 0, 0]
+		return get3DmatrixFrom2D(tempStringMatrix.slice(7,-1).split(',').map(a=>parseFloat(a))); 
+	} else {
+		return [1,0,0,0 ,0,1,0,0 ,0,0,1,0 ,0,0,0,1]
+	}	
+}
+
+const getMatrixFromArray = (A)=>`matrix3d(${A.join(',')})`;
+
+const getRotationMatrixFromAllMatrix = (A)=>[[A[0],A[1],A[2]],[A[4],A[5],A[6]],[A[8],A[9],A[10]]];
+
+function setLayersVisibilityByRotateMatrix(rotateMatrix) {
+	// console.log(`[[${roundM2D(rotateMatrix).join('],[')}]]*[[0],[0],[1]] = [[${math.multiply(roundM2D(rotateMatrix),[0,0,1]).join('],[')}]]`)
+	const currZoneIs = document.querySelectorAll('.currentLayersIs')
+	// console.log( math.multiply(rotateMatrix,[0,0,-1]).map(a=>Math.abs(a)),math.multiply(rotateMatrix,[0,0,-1]).map(a=>Math.sign(a)) )
+	// console.log( math.multiply(rotateMatrix,[0,0,1] ).map(a=>Math.abs(a)),math.multiply(rotateMatrix,[0,0,1] ).map(a=>Math.sign(a)) )
+	const tempVec = math.multiply(rotateMatrix,[0,0,1]).map(a=>Math.abs(a))
+
+	const surf = ['sagittal','frontal','axial']
+	surf.forEach((sur,i)=>{
+		document.querySelector(`#curSurface > #${sur}`).textContent = `${(2*Math.asin(tempVec[i])/Math.PI*100).toFixed(0).padStart(3)}% ${sur.padStart(8)}`
+	})
+
+	switch (tempVec.reduce((a,c,i,A)=>(c>A[a]?i:a),0)) {
+		case 0: document.getElementById('zone3d').setAttribute('style','--lX: auto;'); currZoneIs.forEach(el=>{el.textContent = 'X layers'}); break;
+		case 1: document.getElementById('zone3d').setAttribute('style','--lY: auto;'); currZoneIs.forEach(el=>{el.textContent = 'Y layers'}); break;
+		case 2: document.getElementById('zone3d').setAttribute('style','--lZ: auto;'); currZoneIs.forEach(el=>{el.textContent = 'Z layers'}); break;
+	}
+}
+
+function noZoomMatrix(A) {
+	A[12] = 0
+	A[13] = 0
+	A[14] = 0
+	A[15] = 1;
+	return A;
+}
+
 document.querySelector('#animate #AutoSpin').addEventListener('click',()=>{
 	const Block3D = Zone3Delement.querySelector('#block3d');
 	const Box3D = document.querySelector('#box3d');
 
-	const get3DmatrixFrom2D = (A)=>[A[0],A[1],0,0 ,A[2],A[3],0,0 ,0,0,1,0 ,A[4],A[5],0,1]
-
-	function get3DMatrixTransform(el) {
-		const tempStringMatrix = getComputedStyle(el)['transform']
-		if (tempStringMatrix.includes('3d')) {
-			//              'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)' -> [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-			return tempStringMatrix.slice(9,-1).split(',').map(a=>parseFloat(a));
-		} else if (tempStringMatrix != 'none') {
-			//              'matrix(1, 0, 0, 1, 0, 0)' -> [1, 0, 0, 1, 0, 0]
-			return get3DmatrixFrom2D(tempStringMatrix.slice(7,-1).split(',').map(a=>parseFloat(a))); 
-		} else {
-			return [1,0,0,0 ,0,1,0,0 ,0,0,1,0 ,0,0,0,1]
-		}	
-	}
-
 	const roundM2D = (A,n=4)=>A.map(a=>a.map(aa=>Math.round(aa*10**n)/10**n));
 	const rMZa_90 = roundM2D( math.rotationMatrix(-math.pi / 2, [0, 0, 1]) )
-	const getRotationMatrixFromAllMatrix = (A)=>[[A[0],A[1],A[2]],[A[4],A[5],A[6]],[A[8],A[9],A[10]]];
+	
 	const setNewRotationToOldMatrix = (N,O)=>[N[0][0],N[0][1],N[0][2],O[3],N[1][0],N[1][1],N[1][2],O[7],N[2][0],N[2][1],N[2][2],O[11],O[12],O[13],O[14],O[15]];
 
 	function getNewMatrixAfterAnyRotate(matrix,mmx,mmy,angleF) {
@@ -59,34 +90,6 @@ document.querySelector('#animate #AutoSpin').addEventListener('click',()=>{
 		);
 	}
 
-	function setLayersVisibilityByRotateMatrix(rotateMatrix) {
-		// console.log(`[[${roundM2D(rotateMatrix).join('],[')}]]*[[0],[0],[1]] = [[${math.multiply(roundM2D(rotateMatrix),[0,0,1]).join('],[')}]]`)
-		const currZoneIs = document.querySelectorAll('.currentLayersIs')
-		// console.log( math.multiply(rotateMatrix,[0,0,-1]).map(a=>Math.abs(a)),math.multiply(rotateMatrix,[0,0,-1]).map(a=>Math.sign(a)) )
-		// console.log( math.multiply(rotateMatrix,[0,0,1] ).map(a=>Math.abs(a)),math.multiply(rotateMatrix,[0,0,1] ).map(a=>Math.sign(a)) )
-		const tempVec = math.multiply(rotateMatrix,[0,0,1]).map(a=>Math.abs(a))
-
-		const surf = ['sagittal','frontal','axial']
-		surf.forEach((sur,i)=>{
-			document.querySelector(`#curSurface > #${sur}`).textContent = `${(2*Math.asin(tempVec[i])/Math.PI*100).toFixed(0).padStart(3)}% ${sur.padStart(8)}`
-		})
-
-		switch (tempVec.reduce((a,c,i,A)=>(c>A[a]?i:a),0)) {
-			case 0: document.getElementById('zone3d').setAttribute('style','--lX: auto;'); currZoneIs.forEach(el=>{el.textContent = 'X layers'}); break;
-			case 1: document.getElementById('zone3d').setAttribute('style','--lY: auto;'); currZoneIs.forEach(el=>{el.textContent = 'Y layers'}); break;
-			case 2: document.getElementById('zone3d').setAttribute('style','--lZ: auto;'); currZoneIs.forEach(el=>{el.textContent = 'Z layers'}); break;
-		}
-	}
-
-	const getMatrixFromArray = (A)=>`matrix3d(${A.join(',')})`;
-
-	function noZoomMatrix(A) {
-		A[12] = 0
-		A[13] = 0
-		A[14] = 0
-		A[15] = 1;
-		return A;
-	}
 	if (window.AutoSpinSetInterval) {
 		clearInterval(window.AutoSpinSetInterval)
 	}
@@ -123,6 +126,100 @@ initShowContoursEvents()
 
 initZone3Devents()
 
-if (window.location.search.slice(1) === '0') {
-	openOneFile( fetch("https://passvvord.github.io/test/test_12/testData/фрагмент щелепи.dcm").then(r=>r.arrayBuffer()) )
+
+
+function getHueVis(min,max) {
+	const dens = new Array(5).fill().map((a,i,A)=>min+(max-min)*i/(A.length-1))
+	// const col  = [
+	// 	255,0  ,0  ,255,
+	// 	255,255,0  ,255,
+	// 	0  ,255,0  ,255,
+	// 	0  ,255,255,255,
+	// 	0  ,  0,255,255
+	// ]
+	const col  = [
+		0  ,  0,255,255*0/4,
+		0  ,255,255,255*1/4,
+		0  ,255,0  ,255*2/4,
+		255,255,0  ,255*3/4,
+		255,0  ,0  ,255*4/4
+	]
+	return new Array(4).fill().map((a,i)=>{return {min: dens[i], max: dens[i+1], gradient: true, rgba0: col.slice(i*4,(i+1)*4), rgba1: col.slice((i+1)*4,(i+2)*4)} })
+
+	// [
+	// 	{min: 1/4, max: 7/7, gradient: true, rgba0: [255,0  ,0  ,255], rgba1: [255,255,0  ,255]},
+	// 	{min: 2/4, max: 7/7, gradient: true, rgba0: [255,255,0  ,255], rgba1: [0  ,255,0  ,255]},
+	// 	{min: 3/4, max: 7/7, gradient: true, rgba0: [0  ,255,0  ,255], rgba1: [0  ,255,255,255]},
+	// 	{min: 2/4, max: 7/7, gradient: true, rgba0: [0  ,255,255,255], rgba1: [0  ,  0,255,255]}
+	// ]	
 }
+
+function setMatrix(matrix) {
+	setLayersVisibilityByRotateMatrix(getRotationMatrixFromAllMatrix(matrix));
+	Zone3Delement.querySelector('#block3d').style.transform = getMatrixFromArray(matrix);
+	document.querySelector('#box3d').style.transform = getMatrixFromArray(noZoomMatrix(matrix));	
+}
+
+function setVisPar(params) {
+	setVisParams(
+		params
+		, window.zone3Dparams.min
+		, window.zone3Dparams.max
+	)
+	updateZone3DbyChangingVisParams()
+}
+//getHueVis(400, 4143)
+
+switch ( window.location.search.slice(1,2) ) {
+	case '0':
+		openOneFile( fetch('https://passvvord.github.io/test/test_12/testData/фрагмент щелепи.dcm').then(r=>r.arrayBuffer())
+			,[
+				{min: 300, max: 3000, gradient: true, rgba0: [0,0,40,0], rgba1: [255,255,255,255]},
+				{min: 2300, max: 4143, gradient: true, rgba0: [255,255,0,255], rgba1: [255,0,0,255]},
+			]
+			,{visible: false, X0: 24, X1: 227, Y0: 28, Y1: 225, Z0: 0, Z1: 232}
+			,()=>{
+				setMatrix([
+					 0.877529 , -0.151226, -0.455066, 0,
+					-0.47939  , -0.299983, -0.824755, 0,
+					 0.0117847, -0.941882,  0.335733, 0,
+					 0        ,  0       ,  0       , 1
+				])
+				if ( window.location.search.slice(2,3) === 'a' ) {
+					setMatrix([0.314884, 0.0937909, 0.94448, 0, 0.949092, -0.0421687, -0.312235, 0, -0.0105255, -0.99469, 0.102304, 0, 0, 0, 0, 1])
+					animateSlider(26,224,a=>{setHideLayers({X1: a})},8)
+				}
+		})		
+		break;
+	case '1':
+		openManyFiles( new Array(227).fill().map( (a,i)=>fetch('https://passvvord.github.io/test/test_12/testData/грудна клітина/I'+(i+1).toString().padStart(5,'0')).then(r=>r.arrayBuffer()) )
+			,[
+				{min: 100, max: 1000, gradient: true, rgba0: [0,0,0,0], rgba1: [255,255,255,255]},
+				{min: 1000, max: 2440, gradient: false, rgba0: [255,255,255,255], rgba1: [255,255,255,255]},
+				{min: -1000, max: -600, gradient: true, rgba0: [0,0,255,5], rgba1: [255,0,255,5]},
+				{min: -600, max: -300, gradient: true, rgba0: [255,255,0,100], rgba1: [255,0,0,100]}
+			]
+			,{visible: false, X0: 14, X1: 469, Y0: window.location.search.slice(2,3) === 'a'? 100 : 171, Y1: 394, Z0: 0, Z1: 202}
+			,()=>{
+				setMatrix([0.948241, -0.101548, -0.300886, 0, -0.316661, -0.373151, -0.872051, 0, 0.0237156, -0.922198, 0.386003, 0, 0, 0, 0, 1])
+
+				if ( window.location.search.slice(2,3) === 'a' ) {
+					setMatrix([0.982168, -0.0946957, -0.162377, 0, -0.181791, -0.258781, -0.948699, 0, -0.0478056, -0.961282, 0.271363, 0, 0, 0, 0, 1])
+					animateSlider(100,357,a=>{setHideLayers({Y0: a})},10)
+				}
+			// {visible: false, X0: 14, X1: 469, Y0: 171, Y1: 394, Z0: 0, Z1: 202}
+			// setChoseZone(params)
+		})
+		break;
+	// case '2':
+		
+	// 	break;
+	// case '3':
+		
+	// 	break;
+	default:
+		consoleOut('default mode')
+		break;
+}
+
+
--- test/test_12/js/tools/openFile.js
+++ test/test_12/js/tools/openFile.js
@@ -11,7 +11,7 @@ function get3DarrayFrom1D(data,X,Y,Z) {
 	return res
 }
 
-function openOneFile( arrayBufferPromise ) {
+function openOneFile( arrayBufferPromise, visPar = false, choseZ = false, func = ()=>{} ) {
 	const openfiletampdate = Date.now();
 	draw_preloader()
 	consoleOut('start opening one Dicom File -------------------------------------------')
@@ -52,22 +52,21 @@ function openOneFile( arrayBufferPromise ) {
 			{
 				X: X, Y: Y, Z: Z, 
 				min: min, max: max, 
-				VisParams: (max > 4000?[
+				VisParams: visPar === false ? (max > 4000?[
 						{min: 1000, max: 4000, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]},
 						{min: 4000, max: max, gradient: true, rgba0: [255, 255, 0, 255], rgba1: [255, 0, 0, 255]}
 					]:(max > 2500?[
 						{min: 1000, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
 					]:[
 						{min: min, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
-					])
-
-				),
-				ChoseZoneParams: {
-					visible: false, 
-					X0: 0, X1: X, 
-					Y0: 0, Y1: Y, 
-					Z0: 0, Z1: Z
-				}
+					])) : visPar
+				,
+				ChoseZoneParams: choseZ === false ? {
+						visible: false, 
+						X0: 0, X1: X, 
+						Y0: 0, Y1: Y, 
+						Z0: 0, Z1: Z
+					} : choseZ
 			}
 		)
 		// consoleOut(`scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)})`)
@@ -79,7 +78,105 @@ function openOneFile( arrayBufferPromise ) {
 		consoleOut('end opening one Dicom File ---------------------------------------------')
 		remove_preloader()
 		consoleOut(`file opened and renderd, need time: ${Date.now() - openfiletampdate} ms`)
+		func()
 	})
+}
+
+function openManyFiles( ArrayOfarrayBufferPromise, visPar = false, choseZ = false, func = ()=>{} ) {
+	const openfiletampdate = Date.now();
+	draw_preloader()
+	consoleOut('start opening many Dicom Files -----------------------------------------')
+
+	let tempArrOfImg = new Array(ArrayOfarrayBufferPromise.length)
+	let tempArrOfImgStatus = (new Array(ArrayOfarrayBufferPromise.length)).fill(false)
+	// consoleOut(c.target.files)
+	consoleOut(ArrayOfarrayBufferPromise.length)
+	for (let i = 0; i < ArrayOfarrayBufferPromise.length; i++) {
+		ArrayOfarrayBufferPromise[i].then(f=>{
+	    	tempArrOfImg[i] = daikon.Series.parseImage(new DataView(f))
+	    	tempArrOfImgStatus[i] = true;
+	    	consoleOut(tempArrOfImgStatus.map(a=>(a?'■':'□')).join(''))
+	    	if (tempArrOfImgStatus.reduce((a,b)=>(a?b:false),true)) {
+				consoleOut('succes')
+
+				// consoleOut(tempArrOfImg)
+				const X = tempArrOfImg[0].getCols();
+				const Y = tempArrOfImg[0].getRows();
+				
+				const minFrameNum = tempArrOfImg.reduce((a,b)=>(b.getImageNumber()<a.getImageNumber()?b:a)).getImageNumber()
+				const maxFrameNum = tempArrOfImg.reduce((a,b)=>(b.getImageNumber()>a.getImageNumber()?b:a)).getImageNumber()
+				consoleOut('minFrameNum',minFrameNum,'maxFrameNum',maxFrameNum)
+				
+				const Z = maxFrameNum - minFrameNum + 1;
+
+				consoleOut(X,Y,Z)
+
+				if (Z != tempArrOfImg.length) {throw `there are not enough frames \nyou give: ${tempArrOfImg.map(a=>a.getImageNumber())}`}
+
+				let pixelData = new Int16Array(X*Y*Z)
+
+				for (let i = 0; i < tempArrOfImg.length; i++) {
+					const pos = tempArrOfImg[i].getImageNumber()-minFrameNum
+					const tempPixelData = tempArrOfImg[i].getInterpretedData()
+
+					for (let j = 0; j < tempPixelData.length; j++) {
+						pixelData[pos*tempPixelData.length+j]=tempPixelData[j];
+					}
+				}
+				const min = pixelData.reduce((a,b)=>(b<a?b:a));
+				const max = pixelData.reduce((a,b)=>(b>a?b:a));
+				
+				const orient = tempArrOfImg[0].getOrientation()
+
+				consoleOut(`${X} ${Y} ${Z} ${min} ${max} PixelSpacing: ${tempArrOfImg[0].getPixelSpacing()}, Orientation: ${tempArrOfImg[0].getOrientation()}, PatientID: ${tempArrOfImg[0].getPatientID()}`)
+				updateFileInfo({'X':X+'px','Y':Y+'px','Z':Z+'px','image min':min,'image max':max})
+
+				const tempDate = Date.now()
+				window.hist = new HistForBigIntData(
+					document.querySelector('#histCanv')
+					,pixelData
+					,min
+					,max
+					,50
+				)
+				consoleOut(`histogram calced need time: ${Date.now() - tempDate} ms`)
+				initHistogram(min,max,50)
+				initShowContours(min,max)
+				// setShowContours({detalised: 1, allowInMin: min, allowInMax: max})
+
+				calcBlock3D(
+					get3DarrayFrom1D(pixelData,X,Y,Z),
+					{
+						X: X, Y: Y, Z: Z, 
+						min: min, max: max, 
+						VisParams: visPar === false ? (max > 4000?[
+								{min: 1000, max: 4000, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]},
+								{min: 4000, max: max, gradient: true, rgba0: [255, 255, 0, 255], rgba1: [255, 0, 0, 255]}
+							]:(max > 2500?[
+								{min: 1000, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
+							]:[
+								{min: min, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
+							])) : visPar
+						,
+						ChoseZoneParams: choseZ === false ? {
+								visible: false, 
+								X0: 0, X1: X, 
+								Y0: 0, Y1: Y, 
+								Z0: 0, Z1: Z
+							} : choseZ
+					}
+				)
+				// consoleOut(`scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)})`)
+				document.getElementById('block3d').style.transform = `scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)*-1})`
+				document.getElementById('zone3d').setAttribute('style','--lZ: auto;');
+
+				consoleOut('end opening many Dicom Files -------------------------------------------')
+				remove_preloader()
+				consoleOut(`file opened and renderd, need time: ${Date.now() - openfiletampdate} ms`)
+				func()
+			}
+		})			
+	}
 }
 
 function initOpenFile(element = openFileElement) {
@@ -87,173 +184,105 @@ function initOpenFile(element = openFileElement) {
 
 	element.querySelector('#oneDicomFile').addEventListener('change', c => {
 		openOneFile( c.target.files[0].arrayBuffer() )
-		// const openfiletampdate = Date.now();
-		// draw_preloader()
-		// consoleOut('start opening one Dicom File -------------------------------------------')
-		// c.target.files[0].arrayBuffer().then(f=>{
-	    // 	let image = daikon.Series.parseImage(new DataView(f))
-		// 	const pixelData = new Int16Array(image.getInterpretedData())
-
-		// 	consoleOut('image opened')
-
-		// 	const X = image.getCols();
-		// 	const Y = image.getRows();
-		// 	const Z = image.getNumberOfFrames() //Math.floor(pixelData.length/X/Y);
-		// 	if (Z != Math.floor(pixelData.length/X/Y)) {consoleOut('data problems while reading')}
-		// 	const min = pixelData.reduce((a,b)=>(b<a?b:a));
-		// 	const max = pixelData.reduce((a,b)=>(b>a?b:a));
-		// 	// consoleOut(X,Y,Z,min,max)
-
-		// 	const orient = image.getOrientation()
-		// 	// consoleOut(X,Y,Z,min,max,`PixelSpacing: ${image.getPixelSpacing()}, Orientation: ${image.getOrientation()}, PatientID: ${image.getPatientID()}`)
-		// 	consoleOut(`${X} ${Y} ${Z} ${min} ${max} PixelSpacing: ${image.getPixelSpacing()}, Orientation: ${orient}, PatientID: ${image.getPatientID()}`)
-		// 	updateFileInfo({'X':X+'px','Y':Y+'px','Z':Z+'px','image min':min,'image max':max})
-			
-		// 	const tempDate = Date.now()
-		// 	window.hist = new HistForBigIntData(
-		// 		document.querySelector('#histCanv')
-		// 		,pixelData
-		// 		,min
-		// 		,max
-		// 		,50
-		// 	)
-		// 	consoleOut(`histogram calced need time: ${Date.now() - tempDate} ms`)
-		// 	initHistogram(min,max,50)
-		// 	initShowContours(min,max)
-		// 	// setShowContours({detalised: 1, allowInMin: min, allowInMax: max})
-
-		// 	calcBlock3D(
-		// 		get3DarrayFrom1D(pixelData,X,Y,Z),
-		// 		{
-		// 			X: X, Y: Y, Z: Z, 
-		// 			min: min, max: max, 
-		// 			VisParams: (max > 4000?[
-		// 					{min: 1000, max: 4000, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]},
-		// 					{min: 4000, max: max, gradient: true, rgba0: [255, 255, 0, 255], rgba1: [255, 0, 0, 255]}
-		// 				]:(max > 2500?[
-		// 					{min: 1000, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
-		// 				]:[
-		// 					{min: min, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
-		// 				])
-
-		// 			),
-		// 			ChoseZoneParams: {
-		// 				visible: false, 
-		// 				X0: 0, X1: X, 
-		// 				Y0: 0, Y1: Y, 
-		// 				Z0: 0, Z1: Z
-		// 			}
-		// 		}
-		// 	)
-		// 	// consoleOut(`scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)})`)
-		// 	if (orient) {
-		// 		document.getElementById('block3d').style.transform = `scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)*-1})`
-		// 	}
-		// 	document.getElementById('zone3d').setAttribute('style','--lZ: auto;');
-			
-		// 	consoleOut('end opening one Dicom File ---------------------------------------------')
-		// 	remove_preloader()
-		// 	consoleOut(`file opened and renderd, need time: ${Date.now() - openfiletampdate} ms`)
-		// })
-		
 	})
 
 	element.querySelector('#manyDicomFiles').addEventListener('change', c => {
-		const openfiletampdate = Date.now();
-		draw_preloader()
-		consoleOut('start opening many Dicom Files -----------------------------------------')
+		openManyFiles( new Array(c.target.files.length).fill().map( (a,i)=>c.target.files[i].arrayBuffer() ) )
+		// const openfiletampdate = Date.now();
+		// draw_preloader()
+		// consoleOut('start opening many Dicom Files -----------------------------------------')
 
-		let tempArrOfImg = new Array(c.target.files.length)
-		let tempArrOfImgStatus = (new Array(c.target.files.length)).fill(false)
-		consoleOut(c.target.files)
-		consoleOut(c.target.files.length)
-		for (let i = 0; i < c.target.files.length; i++) {
-			c.target.files[i].arrayBuffer().then(f=>{
-		    	tempArrOfImg[i] = daikon.Series.parseImage(new DataView(f))
-		    	tempArrOfImgStatus[i] = true;
-		    	consoleOut(tempArrOfImgStatus.map(a=>(a?'■':'□')).join(''))
-		    	if (tempArrOfImgStatus.reduce((a,b)=>(a?b:false),true)) {
-					consoleOut('succes')
+		// let tempArrOfImg = new Array(c.target.files.length)
+		// let tempArrOfImgStatus = (new Array(c.target.files.length)).fill(false)
+		// consoleOut(c.target.files)
+		// consoleOut(c.target.files.length)
+		// for (let i = 0; i < c.target.files.length; i++) {
+		// 	c.target.files[i].arrayBuffer().then(f=>{
+		//     	tempArrOfImg[i] = daikon.Series.parseImage(new DataView(f))
+		//     	tempArrOfImgStatus[i] = true;
+		//     	consoleOut(tempArrOfImgStatus.map(a=>(a?'■':'□')).join(''))
+		//     	if (tempArrOfImgStatus.reduce((a,b)=>(a?b:false),true)) {
+		// 			consoleOut('succes')
 
-					// consoleOut(tempArrOfImg)
-					const X = tempArrOfImg[0].getCols();
-					const Y = tempArrOfImg[0].getRows();
+		// 			// consoleOut(tempArrOfImg)
+		// 			const X = tempArrOfImg[0].getCols();
+		// 			const Y = tempArrOfImg[0].getRows();
 					
 					
 
-					const minFrameNum = tempArrOfImg.reduce((a,b)=>(b.getImageNumber()<a.getImageNumber()?b:a)).getImageNumber()
-					const maxFrameNum = tempArrOfImg.reduce((a,b)=>(b.getImageNumber()>a.getImageNumber()?b:a)).getImageNumber()
-					consoleOut('minFrameNum',minFrameNum,'maxFrameNum',maxFrameNum)
+		// 			const minFrameNum = tempArrOfImg.reduce((a,b)=>(b.getImageNumber()<a.getImageNumber()?b:a)).getImageNumber()
+		// 			const maxFrameNum = tempArrOfImg.reduce((a,b)=>(b.getImageNumber()>a.getImageNumber()?b:a)).getImageNumber()
+		// 			consoleOut('minFrameNum',minFrameNum,'maxFrameNum',maxFrameNum)
 					
-					const Z = maxFrameNum - minFrameNum + 1;
+		// 			const Z = maxFrameNum - minFrameNum + 1;
 
-					consoleOut(X,Y,Z)
+		// 			consoleOut(X,Y,Z)
 
-					if (Z != tempArrOfImg.length) {throw `there are not enough frames \nyou give: ${tempArrOfImg.map(a=>a.getImageNumber())}`}
+		// 			if (Z != tempArrOfImg.length) {throw `there are not enough frames \nyou give: ${tempArrOfImg.map(a=>a.getImageNumber())}`}
 
-					let pixelData = new Int16Array(X*Y*Z)
+		// 			let pixelData = new Int16Array(X*Y*Z)
 
-					for (let i = 0; i < tempArrOfImg.length; i++) {
-						const pos = tempArrOfImg[i].getImageNumber()-minFrameNum
-						const tempPixelData = tempArrOfImg[i].getInterpretedData()
+		// 			for (let i = 0; i < tempArrOfImg.length; i++) {
+		// 				const pos = tempArrOfImg[i].getImageNumber()-minFrameNum
+		// 				const tempPixelData = tempArrOfImg[i].getInterpretedData()
 
-						for (let j = 0; j < tempPixelData.length; j++) {
-							pixelData[pos*tempPixelData.length+j]=tempPixelData[j];
-						}
-					}
-					const min = pixelData.reduce((a,b)=>(b<a?b:a));
-					const max = pixelData.reduce((a,b)=>(b>a?b:a));
+		// 				for (let j = 0; j < tempPixelData.length; j++) {
+		// 					pixelData[pos*tempPixelData.length+j]=tempPixelData[j];
+		// 				}
+		// 			}
+		// 			const min = pixelData.reduce((a,b)=>(b<a?b:a));
+		// 			const max = pixelData.reduce((a,b)=>(b>a?b:a));
 					
-					const orient = tempArrOfImg[0].getOrientation()
+		// 			const orient = tempArrOfImg[0].getOrientation()
 
-					consoleOut(`${X} ${Y} ${Z} ${min} ${max} PixelSpacing: ${tempArrOfImg[0].getPixelSpacing()}, Orientation: ${tempArrOfImg[0].getOrientation()}, PatientID: ${tempArrOfImg[0].getPatientID()}`)
-					updateFileInfo({'X':X+'px','Y':Y+'px','Z':Z+'px','image min':min,'image max':max})
+		// 			consoleOut(`${X} ${Y} ${Z} ${min} ${max} PixelSpacing: ${tempArrOfImg[0].getPixelSpacing()}, Orientation: ${tempArrOfImg[0].getOrientation()}, PatientID: ${tempArrOfImg[0].getPatientID()}`)
+		// 			updateFileInfo({'X':X+'px','Y':Y+'px','Z':Z+'px','image min':min,'image max':max})
 
-					const tempDate = Date.now()
-					window.hist = new HistForBigIntData(
-						document.querySelector('#histCanv')
-						,pixelData
-						,min
-						,max
-						,50
-					)
-					consoleOut(`histogram calced need time: ${Date.now() - tempDate} ms`)
-					initHistogram(min,max,50)
-					initShowContours(min,max)
-					// setShowContours({detalised: 1, allowInMin: min, allowInMax: max})
+		// 			const tempDate = Date.now()
+		// 			window.hist = new HistForBigIntData(
+		// 				document.querySelector('#histCanv')
+		// 				,pixelData
+		// 				,min
+		// 				,max
+		// 				,50
+		// 			)
+		// 			consoleOut(`histogram calced need time: ${Date.now() - tempDate} ms`)
+		// 			initHistogram(min,max,50)
+		// 			initShowContours(min,max)
+		// 			// setShowContours({detalised: 1, allowInMin: min, allowInMax: max})
 
-					calcBlock3D(
-						get3DarrayFrom1D(pixelData,X,Y,Z),
-						{
-							X: X, Y: Y, Z: Z, 
-							min: min, max: max, 
-							VisParams: (max > 4000?[
-									{min: 1000, max: 4000, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]},
-									{min: 4000, max: max, gradient: true, rgba0: [255, 255, 0, 255], rgba1: [255, 0, 0, 255]}
-								]:(max > 2500?[
-									{min: 1000, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
-								]:[
-									{min: min, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
-								])
+		// 			calcBlock3D(
+		// 				get3DarrayFrom1D(pixelData,X,Y,Z),
+		// 				{
+		// 					X: X, Y: Y, Z: Z, 
+		// 					min: min, max: max, 
+		// 					VisParams: (max > 4000?[
+		// 							{min: 1000, max: 4000, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]},
+		// 							{min: 4000, max: max, gradient: true, rgba0: [255, 255, 0, 255], rgba1: [255, 0, 0, 255]}
+		// 						]:(max > 2500?[
+		// 							{min: 1000, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
+		// 						]:[
+		// 							{min: min, max: max, gradient: true, rgba0: [0, 0, 0, 0], rgba1: [255, 255, 255, 255]}
+		// 						])
 
-							),
-							ChoseZoneParams: {
-								visible: false, 
-								X0: 0, X1: X, 
-								Y0: 0, Y1: Y, 
-								Z0: 0, Z1: Z
-							}
-						}
-					)
-					// consoleOut(`scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)})`)
-					document.getElementById('block3d').style.transform = `scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)*-1})`
-					document.getElementById('zone3d').setAttribute('style','--lZ: auto;');
+		// 					),
+		// 					ChoseZoneParams: {
+		// 						visible: false, 
+		// 						X0: 0, X1: X, 
+		// 						Y0: 0, Y1: Y, 
+		// 						Z0: 0, Z1: Z
+		// 					}
+		// 				}
+		// 			)
+		// 			// consoleOut(`scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)})`)
+		// 			document.getElementById('block3d').style.transform = `scale3d(${parseInt(orient[3]+1)},${parseInt(orient[4]+1)},${parseInt(orient[5]+1)*-1})`
+		// 			document.getElementById('zone3d').setAttribute('style','--lZ: auto;');
 
-					consoleOut('end opening many Dicom Files -------------------------------------------')
-					remove_preloader()
-					consoleOut(`file opened and renderd, need time: ${Date.now() - openfiletampdate} ms`)
-				}
-			})			
-		}
+		// 			consoleOut('end opening many Dicom Files -------------------------------------------')
+		// 			remove_preloader()
+		// 			consoleOut(`file opened and renderd, need time: ${Date.now() - openfiletampdate} ms`)
+		// 		}
+		// 	})			
+		// }
 	})
 }
