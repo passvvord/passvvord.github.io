dd2ddc18be699c262622c39dc78d472484c23bb3
passvvord <60948866+passvvord@users.noreply.github.com>
2024-02-02 22:12:22 +0200

    ...

--- test/test_16/script.js
+++ test/test_16/script.js
@@ -12,6 +12,18 @@ document.querySelector('div.sec').appendChild(recursiveElement(8,'div'))
 document.querySelector('div.min').appendChild(recursiveElement(6,'div'))
 document.querySelector('div.hour').appendChild(recursiveElement(3,'div'))
 
+// document.querySelectorAll('.sec div').forEach((d,i)=>{
+// 	d.classList.add(i%2==0?'c0':'c1')
+// 	console.log( i,d.classList )
+// })
+
+// setTimeout(()=>{
+// 	document.querySelectorAll('sec, .sec div').forEach((d,i)=>{
+// 		const str = getComputedStyle(d).getPropertyValue(i%2==1?'--t1':'--t0')
+// 		console.log( i, eval(str.replaceAll('calc','')), str )
+// 	})
+// }, 1000/60*2)
+
 const intervalID = setInterval(()=>{
 	['Seconds','Minutes','Hours','Milliseconds'].forEach(p=>{
 		document.documentElement.style.setProperty(`--${p.toLowerCase()}`, new Date()['get'+p]() );
